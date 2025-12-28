3aa64373df64caaaad43d7d7b919c195900e5873
5uvin <suvinv37@gmail.com>
2025-12-29 05:27:06 +0530

    Add spam message detection functionality

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open("spam_model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+st.title("Spam Message Detector")
+
+message = st.text_area("Enter a message:")
+
+if st.button("Predict"):
+    data = vectorizer.transform([message])
+    prediction = model.predict(data)[0]
+    
+    if prediction == 1:
+        st.error("🚨 Spam Message")
+    else:
+        st.success("✅ Not Spam")
